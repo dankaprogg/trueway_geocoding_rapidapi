513c883035502ea3d0c798c690d2fbb98427e2b1
Daniil Shvedov <dankaprog@gmail.com>
2022-06-20 23:10:45 +0300

    0.2.3

--- setup.py
+++ setup.py
@@ -2,14 +2,12 @@ from setuptools import setup
 
 setup(
     name='trueway_geocoding_rapidapi',
-    version='0.2.2',
+    version='0.2.3',
     description='TrueWay Geocoding API on RapidAPI',
     packages=['trueway_geocoding_rapidapi', 'trueway_geocoding_rapidapi.schemas'],
     author_email='dshvedov@kotspin.ru',
     zip_safe=False,
     author='Daniil Shvedov',
-    url='github package source url',
-    download_url='download link you saved',
     keywords=['rapidapi', 'trueway', 'geocoding'],
     classifiers=[],
 )
